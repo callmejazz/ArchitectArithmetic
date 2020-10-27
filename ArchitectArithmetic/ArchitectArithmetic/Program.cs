366948e09311b08ef7b27e0ebed3fea212dd6577
Jasmine Meade <jmeade4347@gmail.com>
2020-10-27 19:34:09 -0400

    Math formula practice

---
+++ ArchitectArithmetic/ArchitectArithmetic/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ArchitectArithmetic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double totalArea = rectangle(2500, 1500) + triangle(500, 750) + 0.5 * circle(375);
+            double totalCost = totalArea * 180;
+            Console.WriteLine(totalCost);
+        }
+        static double rectangle(double length, double width) 
+        {
+            return length * width;
+        
+        }
+        static double circle(double radius)
+        {
+
+            return Math.PI * Math.Pow(radius, 2);
+        }
+        static double triangle(double bottom, double height) 
+        {
+            return 0.5 * bottom * height;
+        }
+    }
+}
